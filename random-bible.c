fe5725d3398b89fe001b74566879ae559cb88933
rodolfobandeira <contato@rodolfobandeira.com>
2017-12-08 20:25:55 -0500

    Assign data_source to one single point

--- random-bible.c
+++ random-bible.c
@@ -7,12 +7,14 @@ int main(void) {
     FILE *fp;
     char * line = NULL;
     char ch;
+    char data_source[12] = "bible.txt";
     int lines = 0;
     int random_line = 0;
     size_t len = 0;
     ssize_t read;
 
-    fp = fopen("bible.txt", "r");
+
+    fp = fopen(data_source, "r");
     while(!feof(fp)) {
       ch = fgetc(fp);
 
@@ -26,7 +28,7 @@ int main(void) {
     random_line = rand() % lines;
 
 
-    fp = fopen("bible.txt", "r");
+    fp = fopen(data_source, "r");
 
     lines = 0;
     while ((read = getline(&line, &len, fp)) != -1) {
